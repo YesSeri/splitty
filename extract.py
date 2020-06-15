525f4634c63cea01d96a3d99b4a681a4378a971d
YesSeri <henrik.zenkert@gmail.com>
2020-06-16 00:53:19 +0200

    Added method for extracting pages from entered pages. Fully functioning now. Bug test with large files left. Check how long time things take. Maybe add save function. Maybe add a pdf viewer built in, but probably not realistic.

---
+++ extract.py
@@ -0,0 +1,57 @@
+import os
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from tkinter import simpledialog
+
+
+def extract(entries, pdf_filename):
+    page_info = get_info(entries)
+    dir_name = create_dir()
+    extract_pages(page_info, pdf_filename, dir_name)
+
+    print(page_info)
+
+# Entries is an array of arrays. The inner array contains three form boxes. [0] = Start page, [1] = end page, [2] = name of aria. 
+
+def get_info(entries):
+    page_info = []
+    for i, entry in enumerate(entries):
+        try:
+            start = int(entry[0].get())
+            end = int(entry[1].get())
+            name = str(i) + ". " + str(entry[2].get())
+            if start > 0 and start < 10000 and end > 0 and end < 10000:
+                page_info.append([start, end, name])
+        except ValueError:
+            break
+    return page_info
+def create_dir():
+    dir_name = simpledialog.askstring("Folder", "Enter output folder name: ")
+    if not os.path.exists("output"):
+        os.mkdir("output")
+        print("Directory " , "output" ,  " Created ")
+    else:
+        print("Directory " , "output" ,  " already exists")
+
+    dir_name = "output/" + dir_name
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+        print("Directory " , dir_name ,  " Created ")
+    else:
+        print("Directory " , dir_name ,  " already exists")
+    return dir_name
+
+def extract_pages(page_info, pdf_filename, dir_name):
+    for info in page_info:
+        extract_one(info, pdf_filename, dir_name)
+
+def extract_one(info, pdf_filename, dir_name):
+    full_dir_name = os.getcwd() + "/" + dir_name
+    with open(pdf_filename, 'rb') as infile:
+        reader = PdfFileReader(infile)
+        if reader.isEncrypted:
+            reader.decrypt('')
+        writer = PdfFileWriter()
+        for i in range(info[0] - 1, info[1]):
+            writer.addPage(reader.getPage(i))
+        with open(full_dir_name + "/" + info[2], 'wb') as outfile:
+            writer.write(outfile)
--- main.py
+++ main.py
@@ -1,66 +1,64 @@
-import os
+from extract import *
 import tkinter as tk
 from tkinter import filedialog
 
-root = tk.Tk()
-root.title("Splitty")
-root.geometry("500x500+0+0")
-frame = tk.Frame(root)
-frame.grid(row=0, column=0)
-side_frame = tk.Frame(root)
-side_frame.grid(row=0, column=1)
-
-
 # Global Variables
-row = 1
+row = 2
 entries = []
 pdf_filename = ""
+
+root = tk.Tk()
+root.title("Splitty")
+root.geometry("1010x1010+0+0")
+frame0 = tk.Frame(root)
+frame1 = tk.Frame(root)
+frame0.grid(row=0, column=0)
+frame1.grid(row=2, column=0)
+#root.grid_rowconfigure(1, weight=1)
 def add_row():
     global row
-    start_e = tk.Entry(frame, width=5)
-    start_e.grid(row=row, column=0, ipady=3)
-    end_e = tk.Entry(frame, width=5)
-    end_e.grid(row=row, column=1, ipady=3)
-    entries.append([start_e, end_e])
+
+    start_entry = tk.Entry(frame0, width=5)
+    end_entry = tk.Entry(frame0, width=5)
+    name_entry = tk.Entry(frame0, width=100)
+
+    start_entry.grid(row=row, column=0, ipady=3)
+    end_entry.grid(row=row, column=1, ipady=3)
+    name_entry.grid(row=row, column=2, ipady=3)
+
+    entries.append([start_entry, end_entry, name_entry])
     row += 1
-
-def save():
-    output = entries[0][0].get() 
-    page_numbers = []
-    for entry in entries:
-        try:
-            start = int(entry[0].get())
-            end = int(entry[1].get())
-            if start > 0 and start < 10000 and end > 0 and end < 10000:
-                page_numbers.append([start, end])
-        except ValueError:
-            break
-
-    print(page_numbers)
-
-    # Set active PDF
 def select_pdf():
     global pdf_filename
     pdf_filename = filedialog.askopenfilename(initialdir=os.getcwd(), title = "Select file",filetypes = (("pdf files","*.pdf"),("all files","*.*")))
+    pdf_label.config(text=os.path.basename(os.path.normpath(pdf_filename)))
 
 # 10 inital rows. Add more with add button
-for i in range(0,10):
+for i in range(0,2):
     add_row()
 
 # Creating gui
 
-start_label = tk.Label(frame, text="Start")
-end_label = tk.Label(frame, text="End")
-add_button = tk.Button(frame, text="Add", command=add_row, width=5)
-save_button = tk.Button(frame, text="Save", command=save, width=5)
-pdf_button = tk.Button(frame, text="PDF", command=select_pdf, width=5)
+start_label = tk.Label(frame0, text="Start")
+end_label = tk.Label(frame0, text="End")
+name_label = tk.Label(frame0, text="Name")
+
+add_button = tk.Button(frame1, text="Add", command=add_row)
+pdf_button = tk.Button(frame1, text="PDF", command=select_pdf)
+extract_button = tk.Button(frame1, text="Extract", command=lambda: extract(entries, pdf_filename))
+
+pdf_label = tk.Label(frame1, text="PDF")
 
 # Adding gui to grid
 
-save_button.grid(row=0, column=2)
-pdf_button.grid(row=1, column=2)
-add_button.grid(row=2, column=2)
-start_label.grid(row=0, column=1)
-end_label.grid(row=0, column=0)
+start_label.grid(row=1, column=0)
+end_label.grid(row=1, column=1)
+name_label.grid(row=1, column=2)
+
+add_button.grid(row=0, column=0)
+pdf_button.grid(row=0, column=1)
+extract_button.grid(row=0, column=2)
+
+pdf_label.grid(row=0, column=3)
 
 root.mainloop()
